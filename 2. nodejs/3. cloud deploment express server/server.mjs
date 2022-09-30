a5dffabe5bc51390521d02ad7c0a553f65936423
M.Inzamam Malik <malikasinger1@gmail.com>
2022-09-30 21:31:22 +0500

    class code

--- 2. nodejs/3. cloud deploment express server/server.mjs
+++ 2. nodejs/3. cloud deploment express server/server.mjs
@@ -9,7 +9,6 @@ app.get('/', (req, res) => {
     res.send('response from sir inzamam computer: ' +  req.ip)
 })
 
-// 172.16.21.247:3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
