a3753d73b35223068806e470a0b00dde01d0ebae
M.Inzamam Malik <malikasinger1@gmail.com>
2022-10-03 20:06:17 +0500

    class code

--- 2. nodejs/5. todo app with server and database/server.mjs
+++ 2. nodejs/5. todo app with server and database/server.mjs
@@ -1,61 +1,89 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
+
+
+let todoSchema = new mongoose.Schema({
+    text: { type: String, required: true },
+    classId: String,
+    createdOn: { type: Date, default: Date.now }
+});
+const todoModel = mongoose.model('todos', todoSchema);
+
+
 
 const app = express()
 const port = process.env.PORT || 3000;
-
-let todos = [];
-
-
-// // Uniform Interface:
-//=======================
-//     POST /todo
-//     GET  /todos
-//     GET  /todo/:id
-//     PUT  /todo/:id
-//   DELETE /todo/:id
-
-
-
-
 
 
 app.use(express.json());
 app.use(cors());
 
 app.post('/todo', (req, res) => {
-    
-    todos.push(req.body.text);
 
-    res.send({
-        message: "your todo is saved",
-        data: todos
+    todoModel.create({ text: req.body.text }, (err, saved) => {
+        if (!err) {
+            console.log(saved);
+
+            res.send({
+                message: "your todo is saved"
+            })
+        } else {
+            res.status(500).send({
+                message: "server error"
+            })
+        }
     })
 })
 app.get('/todos', (req, res) => {
-    
-    res.send({
-        message: "here is you todo list",
-        data: todos
-    })
-})
-app.post('/water', (req, res) => {
-    console.log('someone is offering water');
-    res.send('ok I have received water')
-})
-app.get('/pizza', (req, res) => {
-    console.log('some one is asking for pizza');
-    res.send('here is your pizza')
-})
-app.get('/coffee', (req, res) => {
-    console.log('some one is asking for coffee');
-    res.send('here is your coffee')
-})
 
-// POST 172.16.18.202:3000/water
-
-// 172.16.18.202:5500
+    todoModel.find({}, (err, data) => {
+        if (!err) {
+            res.send({
+                message: "here is you todo list",
+                data: data
+            })
+        }else{
+            res.status(500).send({
+                message: "server error"
+            })
+        }
+    });
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server app is listening on port ${port}`)
 })
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+let dbURI = 'mongodb+srv://dbuser:dbpassword@cluster0.gq9n2zr.mongodb.net/abcdatabase?retryWrites=true&w=majority';
+mongoose.connect(dbURI);
+
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {//connected
+    console.log("Mongoose is connected");
+    // process.exit(1);
+});
+
+mongoose.connection.on('disconnected', function () {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function (err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+
