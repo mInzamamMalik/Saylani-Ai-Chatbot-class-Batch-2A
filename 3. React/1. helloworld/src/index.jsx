d7d14f489f61134eea2f9299d0dad61a1145db1b
M.Inzamam Malik <malikasinger1@gmail.com>
2022-10-26 19:18:47 +0500

    class code

--- 3. React/1. helloworld/src/index.jsx
+++ 3. React/1. helloworld/src/index.jsx
@@ -4,7 +4,14 @@ import ReactDOM from 'react-dom';
 function Hi() {
   return <div>
 
-    Hello Malik sdfsdfdf
+    Hello Malik
+    <h1> firebase hosting copleted </h1>
+
+    <h2> some morw data in h2</h2>
+
+  
+
+
 
 
 
