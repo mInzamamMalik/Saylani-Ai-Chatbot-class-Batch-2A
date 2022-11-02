964bb85b02cccaf3e0e46742e59872d570df4039
M.Inzamam Malik <malikasinger1@gmail.com>
2022-11-02 19:27:30 +0500

    class code

--- 3. React/3. useEffect/src/index.jsx
+++ 3. React/3. useEffect/src/index.jsx
@@ -12,6 +12,29 @@ function Hi() {
 
 
   const [data, setData] = useState(0);
+  const [cityName, setCityName] = useState("");
+  const [weather, setWeather] = useState({});
+
+
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    console.log("city: " + cityName);
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=e0f99c494c2ce394a18cc2fd3f100543&units=metric`)
+      .then(function (response) {
+
+        console.log("data: ", response.data);
+
+        setWeather(response.data)
+
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }
 
   useEffect(() => {
     const getWeather = () => { // get current weather 
@@ -19,6 +42,7 @@ function Hi() {
         .then(function (response) {
 
           console.log("data: ", response.data);
+          setWeather(response.data)
 
         })
         .catch(function (error) {
@@ -26,9 +50,7 @@ function Hi() {
           console.log(error);
         })
     }
-
     getWeather();
-
   }, [])
 
 
@@ -36,7 +58,7 @@ function Hi() {
   return <div>
 
 
-    <h1>State variable</h1>
+    <div>State variable</div>
     {data}
 
     <button onClick={() => {
@@ -54,6 +76,40 @@ function Hi() {
 
     {(data % 2 === 0) ? <b>Even</b> : <i>Odd</i>}
 
+
+
+    <hr />
+
+
+    <h1>
+      weather app:
+    </h1>
+
+    <form onSubmit={submitHandler}>
+
+      <input
+        type="text"
+        placeholder='enter your city name'
+        onChange={(e) => {
+          setCityName(e.target.value)
+        }}
+      />
+
+      <button type="submit">Get Weather</button>
+
+    </form>
+
+    <br />
+
+    {(weather?.name)?
+
+      <div>
+        <div>Weather of {weather?.name}</div>
+        <div>Current Temp {weather?.main?.temp}</div>
+      </div>
+      :
+      null
+    }
 
   </div>;
 }
